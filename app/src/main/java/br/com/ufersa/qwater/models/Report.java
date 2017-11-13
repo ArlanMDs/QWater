bc01533e9192d851dc996c04a9672656e871e54e
ArlanMDs <ArlanMDs@gmail.com>
2017-11-13 03:36:51 -0300

    adicionado classe de relatório, que vai ser usada para guardar resultados no banco de dados

---
+++ app/src/main/java/br/com/ufersa/qwater/models/Report.java
@@ -0,0 +1,28 @@
+package br.com.ufersa.qwater.models;
+
+
+
+public class Report {
+
+
+    private double correctedRAS, CEa;
+
+
+    public double getCorrectedRAS() {
+        return correctedRAS;
+    }
+
+    public void setCorrectedRAS(double correctedRAS) {
+        this.correctedRAS = correctedRAS;
+    }
+
+    public double getCEa() {
+        return CEa;
+    }
+
+    public void setCEa(double CEa) {
+        this.CEa = CEa;
+    }
+
+
+}
